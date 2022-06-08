07206522cc92f16d9bdef0f66c4cfa4ce456eabd
Yi Hang ZHANG <101088679+Mr-Xiao-Zhang@users.noreply.github.com>
2022-06-08 15:06:13 +0800

    Revert "add style"

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,9 @@ import Layout from './layout/layout';
 import 'antd/dist/antd.variable.less';
 import './font/font.less';
 import { AuthProvider } from './providers/AuthProvider';
-import zhCN from 'antd/lib/locale/zh_CN';
 
 ReactDom.render(
-  <ConfigProvider locale={zhCN}>
+  <ConfigProvider>
     <AuthProvider>
       <Layout />
     </AuthProvider>
